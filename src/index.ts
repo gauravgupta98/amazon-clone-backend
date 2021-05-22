31f16f2865d7027007f0a48d85751a9922526ac3
Gaurav Gupta <gauravgupta1533@gmail.com>
2021-05-22 14:18:48 +0530

    added stripe payments endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+const stripe = require("stripe")(
+  "sk_test_51ItpgLSDXMsfyylsVd40cYscedtTJ2dzOWjav4B6H27zuhRjVy5kdkArjqAE98WOyl1DJcgLXaV2WbAFd4kofV5M00CPhYQbTd"
+);
 
 const app = express();
 
@@ -11,6 +14,19 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (_request, response) =>
   response.status(200).send("Server is up and running!")
 );
+
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "usd",
+  });
+
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
 
 app.listen(PORT, () => {
   return console.log(`server is listening on ${PORT}`);
